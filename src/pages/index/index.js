b888026bb445e8a3996b1ae5ad3274478ce4d37b
Min Zhang <zhangmin@MindeMacBook-Pro.local>
2018-06-20 11:32:56 +0800

    游戏

---
+++ src/pages/index/index.js
@@ -0,0 +1,11 @@
+/*
+ * @Author: Zhang Min 
+ * @Date: 2018-06-13 08:04:58 
+ * @Last Modified by: Zhang Min
+ * @Last Modified time: 2018-06-20 11:22:06
+ */
+
+import Toolkit from '@src/components/toolkit.js';
+import './index.less';
+
+console.log(Toolkit.getUrlParameter('name'));
